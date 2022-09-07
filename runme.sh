2cc4249bd8259f45378fb47909e70d612afa6ced
H6rst0n <104990977+H6rst0n@users.noreply.github.com>
2022-09-07 22:42:08 +0800

    v1.2
    fix the little bug

--- runme.sh
+++ runme.sh
@@ -3,6 +3,7 @@ cd ../
 mv ./scripts_for_vasp $HOME/scripts
 cd $HOME/scripts/scripts_for_vasp
 chmod +x *py *sh
+chmod -x runme.sh
 echo 'export PATH=$HOME/scripts/scripts_for_vasp:$PATH' >> ~/.bashrc
 echo 'module load anaconda3/5.1.10' >> ~/.bashrc
 source ~/.bashrc
