7e77600e1b144cefc55ce7bbc52134a734df3e78
H6rst0n <104990977+H6rst0n@users.noreply.github.com>
2022-09-07 22:09:30 +0800

    v1.1
    more easy to use

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 mkdir -p $HOME/scripts
-cd $HOME/scripts
-git clone https://github.com/H6rst0n/scripts_for_vasp.git
-cd ./scripts_for_vasp
+cd ../
+mv ./scripts_for_vasp $HOME/scripts
+cd $HOME/scripts/scripts_for_vasp
 chmod +x *py *sh
 echo 'export PATH=$HOME/scripts/scripts_for_vasp:$PATH' >> ~/.bashrc
 echo 'module load anaconda3/5.1.10' >> ~/.bashrc
